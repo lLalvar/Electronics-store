06304285c6ac06b458d4fcb58c4c16df905deb05
ILalvar <mnacakanyan.lalvar@gmail.com>
2022-07-26 12:44:54 +0400

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://lalvar-electronics-store.netlify.app/',
   plugins: [vue()],
   server: {
     host: true,
